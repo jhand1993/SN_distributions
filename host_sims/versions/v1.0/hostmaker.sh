0dd90595339f12f601bd5d35ba1dd29aee1f9549
thelaphone <jared.hand1993@gmail.com>
2016-09-21 13:59:19 -0600

    removed v1, v2 and just put in guts

--- host_sims/versions/v1.0/hostmaker.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-# python3 hostlib_generator_ps1.py
-python3 hostlib_generator_lowz.py
-python3 hostlib_generator_snls.py
-python3 hostlib_generator_sdss.py
--- host_sims/versions/v1.0/libmover.sh
+++
@@ -1,6 +0,0 @@
-# moves hostlib files to SNANA root simlib directory on midway.
-SNROOT="/project/rkessler/SN/SNDATA_ROOT/"
-cp hostlib/sdss/JSH_WFIRST_SDSS.HOSTLIB $SNROOT/simlib/
-cp hostlib/snls/JSH_WFIRST_SNLS.HOSTLIB $SNROOT/simlib/
-# cp hostlib/ps1/JSH_WFIRST_PS1.HOSTLIB $SNROOT/simlib/
-cp hostlib/lowz/JSH_WFIRST_LOWZ.HOSTLIB $SNROOT/simlib/
